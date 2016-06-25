267d60c5b0f025ba4cb56c9199838ed31a07a085
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-06-26 01:40:33 +0200

    Added GUI in game

---
+++ Assets/Scripts/GUI/InGameGUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameGUI : MonoBehaviour {
+
+    public Text scoreText;
+    public Text livesText;
+
+    void OnEnable()
+    {
+        GameController gameController = Toolbox.GameController;
+        gameController.OnScoreChanged += ScoreWasUpdated;
+        gameController.OnLivesChanged += LivesWereUpdated;
+
+        ScoreWasUpdated(gameController);
+        LivesWereUpdated(gameController);
+    }
+
+    void OnDisable()
+    {
+        GameController gameController = Toolbox.GameController;
+        gameController.OnScoreChanged -= ScoreWasUpdated;
+        gameController.OnLivesChanged -= LivesWereUpdated;
+    }
+
+    void ScoreWasUpdated(GameController gameControl)
+    {
+        scoreText.text = gameControl.Score.ToString();
+    }
+
+    void LivesWereUpdated(GameController gameControl)
+    {
+        livesText.text = "x " + gameControl.Lives;
+    }
+}
